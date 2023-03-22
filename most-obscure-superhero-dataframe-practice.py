3e0ee8a5bce5db90388a0d82fb612ea05aad85e4
Yu-Hsuen Liu <69971331+peterliu419@users.noreply.github.com>
2023-03-22 22:00:51 +0800

    spark-sql

---
+++ most-obscure-superhero-dataframe-practice.py
@@ -0,0 +1,24 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+
+spark = SparkSession.builder.appName("MostPopularSuperhero").getOrCreate()
+
+schema = StructType([ \
+                     StructField("id", IntegerType(), True), \
+                     StructField("name", StringType(), True)])
+
+names = spark.read.schema(schema).option("sep", " ").csv("/Users/peter/SparkCourse/Marvel+Names")
+
+lines = spark.read.text("/Users/peter/SparkCourse/Marvel+Graph") # read each line as column: value
+
+# Small tweak vs. what's shown in the video: we trim each line of whitespace as that could
+# throw off the counts.
+connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0]) \
+    .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " ")) - 1) \
+    .groupBy("id").agg(func.sum("connections").alias("connections"))
+    
+mostObscure = connections.filter(func.col("connections")==1)
+mostObscureName = mostObscure.join(names,"id")
+mostObscureName.select("name").show()
+
